6915329f1e845ec22db8a36ba6170a5e710406d9
PatWil <patrick.williams@tlgcohort.com>
2021-04-27 12:57:31 -0700

    commented some html stuff out because the job api does not have all the same parameters

--- jobsfinder.js
+++ jobsfinder.js
@@ -15,35 +15,44 @@ function searchJobs(e) {
 }
 
 //handle displaying of found jobs
-function displayFoundJobs(data){
-    const addTo = document.getElementById("job-card-container");
-    //create cards
-    let jobCard = document.createElement("div");
-    data.foreach((element) => {
-            //job title
-    let jobTile = document.createElement("h2");
-    jobTile.innerHTML = element.title;
-            //company name
-    let companyName = document.createElement("h3");
-    companyName.innerHTML = element.company;
-            //job description
-     let jobDescription = document.createElement("p");
-     jobDescription.innerHTML = element.description;
-            //url to apply to job
-    let urlLink = element.url;
-    let toJob = `window.location.href='${urlLink}';`;
-            //button to go to job post
-     let apply = document.createElement("button");
-     apply.setAttribute("onclick", toJob);
-     apply.innerHTML = "apply";
+function displayFoundJobs(data) {
 
-     //append to card
-    jobCard.appendChild(jobTile);
-    jobCard.appendChild(companyName);
-    jobCard.appendChild(jobDescription);
-    jobCard.appendChild(apply);
+document.getElementById("job-title").innerHTML = data[0].title;
+document.getElementById("company").innerHTML = data[0].company;
+document.getElementById("company-logo").setAttribute("src", data[0].company_logo);
+document.getElementById("job-title").innerHTML = data[0].description;
 
-    //append card to html page
-    addTo.appendChild(jobCard);
-    })
+
+
+
+//   const addTo = document.getElementById("job-card-container");
+//   //create cards
+//   let jobCard = document.createElement("div");
+//   data.foreach((element) => {
+//     //job title
+//     let jobTile = document.createElement("h2");
+//     jobTile.innerHTML = element.title;
+//     //company name
+//     let companyName = document.createElement("h3");
+//     companyName.innerHTML = element.company;
+//     //job description
+//     let jobDescription = document.createElement("p");
+//     jobDescription.innerHTML = element.description;
+//     //url to apply to job
+//     let urlLink = element.url;
+//     let toJob = `window.location.href='${urlLink}';`;
+//     //button to go to job post
+//     let apply = document.createElement("button");
+//     apply.setAttribute("onclick", toJob);
+//     apply.innerHTML = "apply";
+
+//     //append to card
+//     jobCard.appendChild(jobTile);
+//     jobCard.appendChild(companyName);
+//     jobCard.appendChild(jobDescription);
+//     jobCard.appendChild(apply);
+
+//     //append card to html page
+//     addTo.appendChild(jobCard);
+//   });
 }
